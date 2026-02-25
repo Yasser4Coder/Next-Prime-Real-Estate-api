9ecda83743ad631a2fad82cd0ffb637a4e208858
Yasser4Coder <by.benseghier.yasser@gmail.com>
2026-02-26 00:37:55 +0100

    add www.nextprimerealestate.com to allowed origins

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.use('/uploads', express.static(uploadsPath))
 const allowedOrigins = [
   'http://localhost:5173',
   'https://nextprimerealestate.com',
+  'https://www.nextprimerealestate.com',
 ]
 
 app.use(
